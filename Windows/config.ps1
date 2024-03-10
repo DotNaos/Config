ef536edd4aa8f3ba8eefed359a6fe3cee29ba36d
DotNaos <schuetzoliver00@gmail.com>
2024-03-10 23:19:44 +0100

    Adding Programms and Install Script

---
+++ Windows/config.ps1
@@ -0,0 +1,29 @@
+# Changing windows settings
+
+  ## Set Dark mode
+  New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -Type DWord -Force
+
+  ## Set User Account Control to never notify
+  New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name EnableLUA -Value 0 -Type DWord -Force
+
+
+# Configure Windows Terminal
+
+  ## Set pwsh as default in Terminal
+  New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -Value 0 -Type DWord -Force
+
+
+# Configure Taskbar
+
+  ## Remove Search Bar
+  New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name SearchboxTaskbarMode -Value 0 -Type DWord -Force
+
+  ## Show Widgets
+  New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -Value 0 -Type DWord -Force
+
+  ## Show Copilot in Taskbar
+  New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -Value 0 -Type DWord -Force
+
+# Configure Windows Explorer
+
+
---
+++ Windows/install.ps1
@@ -0,0 +1,5 @@
+choco install -y ./packages/misc.config
+choco install -y ./packages/gaming.config
+choco install -y ./packages/cli.config
+choco install -y ./packages/dev.config
+# choco install -y ./packages/python.config
